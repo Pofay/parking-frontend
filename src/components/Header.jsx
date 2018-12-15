257547cb722a09251ea5b3f0bff0dff5fee9a837
pofay <gianCarlo.gilos@gmail.com>
2018-12-15 14:34:14 +0800

    Created dummy components and added infrastructure packages

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import homeImage from '../../assets/header-image.png';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <nav>
+          <img src={homeImage} alt="logo" />
+        </nav>
+      </header>
+    );
+  }
+}
