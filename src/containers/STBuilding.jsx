6ecfba21e84d75d1ede60a9f120b6e50fe5c793a
Pofay <gianCarlo.gilos@gmail.com>
2019-01-03 16:04:22 +0800

    Used react-redux to bind store to container components

--- src/containers/STBuilding.jsx
+++ src/containers/STBuilding.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import Chip from '@material-ui/core/Chip';
-import Avatar from '@material-ui/core/Avatar';
+import { connect } from 'react-redux'
 import ParkingLot from '../components/ParkingLot';
 
-export default class STBuilding extends React.Component {
+const mapStateToProps = state => ({ parkingLots: state.parkingLots})
+
+class STBuilding extends React.Component {
   render() {
     return (
       <div style={{ backgroundColor: '#696969', height: '50%', width: '25%' }}>
-        <ParkingLot lotName="C1" status={1} />
-        <ParkingLot lotName="C2" status={0} />
-        <ParkingLot lotName="C3" status={0} />
+        {this.props.parkingLots.map(x => (<ParkingLot key={x.id} lotName={x.name} status={x.status}/>))}
       </div>
     );
   }
 }
+
+export default connect(mapStateToProps)(STBuilding)
+
+
