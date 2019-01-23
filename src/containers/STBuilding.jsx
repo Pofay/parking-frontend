2780f290aa896f8bdaeeb7ee9c51f36d5bf17105
Pofay <gianCarlo.gilos@gmail.com>
2019-01-23 20:03:08 +0800

    Changed STBuilding layout from class to a function component

--- src/containers/STBuilding.jsx
+++ src/containers/STBuilding.jsx
@@ -26,81 +26,72 @@ const mapStateToProps = state => {
   };
 };
 
-class STBuilding extends React.Component {
-  render() {
-    return (
-      <div
-        style={{
-          backgroundColor: '#696969',
-          display: 'inline',
-          flexGrow: 1,
-          margin: 'auto'
-        }}
-      >
-        <Grid container spacing={16} justify="left">
-          <Grid item xs={2} />
-          <Paper>
-            {' '}
-            Available Slots: {this.props.occupiedSlots} / {this.props.totalSlots}
-          </Paper>
-        </Grid>
-        <Grid style={{paddingTop: '1%' }}container spacing={16} justify="center">
-          <Grid item xs={1}>
-            <Paper style={{ textAlign: 'center', height: '100%' }}>
-              ST Building
-            </Paper>
-          </Grid>
-          <Grid item xs={1}>
-            <Paper style={{ textAlign: 'center', height: '100%' }}>
-              {' '}
-              Motorcycle Area{' '}
-            </Paper>
-          </Grid>
-          <Grid item xs={6}>
-            <Paper elevation={1} style={{ height: '40%' }}>
-              Faculty Area
-            </Paper>
-            <div style={{ marginTop: '2%', display: 'inline-block' }}>
-              {this.props.upperParkingLots.map(x => (
-                <ParkingLot key={x.id} {...x} />
-              ))}
-            </div>
-          </Grid>
-          <Grid item xs={1}>
-            <Paper style={{ height: '100%' }} elevation={1}>
-              Canteen
-            </Paper>
-          </Grid>
-        </Grid>
+const STBuilding = props => (
+  <div
+    style={{
+      backgroundColor: '#696969',
+      display: 'inline',
+      flexGrow: 1,
+      margin: 'auto'
+    }}
+  >
+    <Grid container spacing={16} justify="left">
+      <Grid item xs={2} />
+      <Paper>
+        {' '}
+        Available Slots: {props.occupiedSlots} / {props.totalSlots}
+      </Paper>
+    </Grid>
+    <Grid style={{ paddingTop: '1%' }} container spacing={16} justify="center">
+      <Grid item xs={1}>
+        <Paper style={{ textAlign: 'center', height: '100%' }}>
+          ST Building
+        </Paper>
+      </Grid>
+      <Grid item xs={1}>
+        <Paper style={{ textAlign: 'center', height: '100%' }}>
+          {' '}
+          Motorcycle Area{' '}
+        </Paper>
+      </Grid>
+      <Grid item xs={6}>
+        <Paper elevation={1} style={{ height: '40%' }}>
+          Faculty Area
+        </Paper>
+        <div style={{ marginTop: '2%', display: 'inline-block' }}>
+          {props.upperParkingLots.map(x => (
+            <ParkingLot key={x.id} {...x} />
+          ))}
+        </div>
+      </Grid>
+      <Grid item xs={1}>
+        <Paper style={{ height: '100%' }} elevation={1}>
+          Canteen
+        </Paper>
+      </Grid>
+    </Grid>
 
-        <Grid
-          style={{ paddingTop: '1%' }}
-          container
-          spacing={16}
-          justify="center"
+    <Grid style={{ paddingTop: '1%' }} container spacing={16} justify="center">
+      <Grid item xs={10}>
+        <div style={{ marginTop: '2%', display: 'inline-block' }}>
+          {props.lowerParkingLots.map(x => (
+            <ParkingLot key={x.id} {...x} />
+          ))}
+        </div>
+      </Grid>
+      <Grid item xs={10}>
+        <Paper
+          style={{
+            height: '100%',
+            paddingRight: '10%',
+            paddingLeft: '10%'
+          }}
         >
-          <Grid item xs={10}>
-            <div style={{ marginTop: '2%', display: 'inline-block' }}>
-              {this.props.lowerParkingLots.map(x => (
-                <ParkingLot key={x.id} {...x} />
-              ))}
-            </div>
-          </Grid>
-          <Grid item xs={10}>
-            <Paper
-              style={{
-                height: '100%',
-                paddingRight: '10%',
-                paddingLeft: '10%'
-              }}
-            >
-              Main Building
-            </Paper>
-          </Grid>
-        </Grid>
-      </div>
-    );
-  }
-}
+          Main Building
+        </Paper>
+      </Grid>
+    </Grid>
+  </div>
+);
 
 export default connect(mapStateToProps)(STBuilding);
